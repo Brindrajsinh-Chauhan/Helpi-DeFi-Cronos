e5284ece04aea39bb85b2aa925308e843573c964
teterabOb <gilbertsahumada@gmail.com>
2021-12-07 20:46:06 -0300

    changes in json file

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ dotenv.config()
 //Middleware
 //app.use(morgan('dev'));
 //app.use(express.urlencoded({extended:false}));
-//app.use(express.json());
+app.use(express.json());
 
 
 app.get('/', async(req, res) => {
