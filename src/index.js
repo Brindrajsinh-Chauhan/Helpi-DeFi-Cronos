c452610f52cac5c5a7353029ab1fb83f986af3aa
teterabOb <gilbertsahumada@gmail.com>
2021-12-07 20:32:40 -0300

    changes app

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ dotenv.config()
 
 //Middleware
 //app.use(morgan('dev'));
-app.use(express.urlencoded({extended:false}));
+//app.use(express.urlencoded({extended:false}));
 app.use(express.json());
 
 
