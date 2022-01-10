edcb92cf4c4f4b3f7e8e32975b19e34a4c834f41
Brindrajsinh Chauhan <91854080+Brindrajsinh-Chauhan@users.noreply.github.com>
2022-01-10 16:15:15 -0500

    Add files via upload

--- src/components/Navbars/AdminNavbar.js
+++ src/components/Navbars/AdminNavbar.js
@@ -2,9 +2,11 @@ import React from "react";
 import { useHistory } from "react-router-dom";
 
 import UserDropdown from "components/Dropdowns/UserDropdown.js";
+import networkpill from '../Pills/networkPill.js'
 
 export default function Navbar() {
   const history = useHistory();
+  let networkcontent = networkpill()
   return (
     <>
       {/* Navbar */}
@@ -24,21 +26,21 @@ export default function Navbar() {
           </a>
           {/* Form */}
           <form className="md:flex hidden flex-row flex-wrap items-center lg:ml-auto mr-3">
-            <div className="relative flex w-full flex-wrap items-stretch">
-              {/* <span className="z-10 h-full leading-snug font-normal absolute text-center text-blueGray-300 absolute bg-transparent rounded text-base items-center justify-center w-8 pl-3 py-3">
-                <i className="fas fa-search"></i>
-              </span> */}
-              {/* <input
-                type="text"
-                placeholder="Search here..."
-                className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 relative bg-white bg-white rounded text-sm shadow outline-none focus:outline-none focus:ring w-full pl-10"
-              /> */}
-            </div>
+
           </form>
+
           {/* User */}
-          <ul className="flex-col md:flex-row list-none items-center hidden md:flex">
+
+            <div class="flex flex-col mx-4 rounded-full">
+               <div class="flex mx-auto border-2 border-white-400 shadow-lg rounded-full h-12">
+                {networkcontent}
+                </div>
+            </div>
+
+            <ul className="flex-col md:flex-row list-none items-center hidden md:flex">
             <UserDropdown />
-          </ul>
+            </ul>
+
         </div>
       </nav>
       {/* End Navbar */}
